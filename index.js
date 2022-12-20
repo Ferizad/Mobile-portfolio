2df7f38f5055d58cff98ced78312a25ce8828af3
Ferizad <dossanovafm@gmail.com>
2022-12-20 10:55:06 +0600

    Modified JS file

---
+++ index.js
@@ -0,0 +1,3 @@
+let hamburger_btn = document.querySelector(".hamburger_img")
+let close_hamburger_btn = document.querySelector(".close_hamburger")
+let modal_menu_container = document.querySelector(".hamburger_img")
