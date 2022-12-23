df5c8888c1089f174cec7d2d808ab814437d5ec5
Ferizad <dossanovafm@gmail.com>
2022-12-23 16:02:59 +0600

    Modified HTML, CSS files

--- index.js
+++ index.js
@@ -3,13 +3,19 @@ const closeHamburgerBtn = document.querySelector('.close_hamburger');
 const modalMenuContainer = document.querySelector('.modal_menu_container');
 const menuOption = document.querySelectorAll('.menu_option');
 const worksSection = document.querySelector('.works_section');
+const form = document.querySelector('form');
+const button = document.querySelector('#btn_1');
+const message = document.querySelector('.errorMsg');
+const fName = document.querySelector('#name');
+const email = document.querySelector('#email');
+const textArea = document.querySelector('#text_message');
 
 const cardsData = [
   {
     id: 0,
     name: ['Tonic', 'Tonic'],
     description: [
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent', 'A daily selection of privately personalized reads; no accounts or sign-ups required.'],
+      'A daily selection of privately personalized reads; no accounts or sign-ups required. Lorem Ipsum is simply dummy text of the printing and typesetting industry. A daily selection of privately personalized reads; no accounts or sign-ups required.', 'A daily selection of privately personalized reads; no accounts or sign-ups required. A daily selection of privately personalized reads; no accounts or sign-ups required.'],
     image: ['media/jumbo1.jpg', 'media/jumbo5.png'],
     technologies: ['html', ' css ', 'javaScript'],
     technologiesDesk: ['html', 'Ruby on rails', 'css', 'javaScript'],
@@ -22,7 +28,7 @@ const cardsData = [
     id: 1,
     name: ['Multi-Post Stories', 'Multi-Post Stories'],
     description: [
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent', 'A daily selection of privately personalized reads; no accounts or sign-ups required.', 'Experimental content creation feature that allows users to add to an existing story over the course a day without spamming their friends.'],
+      'A daily selection of privately personalized reads; no accounts or sign-ups required. A daily selection of privately personalized reads; no accounts or sign-ups required.', 'A daily selection of privately personalized reads; no accounts or sign-ups required. A daily selection of privately personalized reads; no accounts or sign-ups required. A daily selection of privately personalized reads; no accounts or sign-ups required.'],
     image: ['media/jumbo4.jpg', 'media/jumbo4b.jpg'],
     technologies: ['html', ' css ', 'javaScript'],
     technologiesDesk: ['html', 'Ruby on rails', 'css', 'javaScript'],
@@ -35,7 +41,7 @@ const cardsData = [
     id: 2,
     name: ['Tonic', 'Facebook 360'],
     description:
-      ['Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent', "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR..."],
+      ['A daily selection of privately personalized reads; no accounts or sign-ups required. A daily selection of privately personalized reads; no accounts or sign-ups required.', "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR... Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR..."],
     image: ['media/jumbo3.jpg', 'media/jumbo1b.png'],
     technologies: ['html', ' css ', 'javaScript'],
     technologiesDesk: ['html', 'Ruby on rails', 'css', 'javaScript'],
@@ -48,7 +54,7 @@ const cardsData = [
     id: 3,
     name: ['Multi-Post Stories', 'Uber Navigation'],
     description:
-      ['Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent', 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.'],
+      ['A daily selection of privately personalized reads; no accounts or sign-ups required. A daily selection of privately personalized reads; no accounts or sign-ups required.', 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car. A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.'],
     image: ['media/jumbo4.jpg', 'media/jumbo2b.png'],
     technologies: ['html', ' css ', 'javaScript'],
     technologiesDesk: ['html', 'Ruby on rails', 'css', 'javaScript'],
@@ -72,53 +78,125 @@ menuOption.forEach((e) => {
   };
 });
 
+const formChangeEvents = [fName, email, textArea];
+formChangeEvents.forEach((eachField) => {
+  eachField.addEventListener('change', () => {
+    message.innerHTML = '';
+    const formDataObject = {
+      nameKey: fName.value,
+      emailKey: email.value,
+      textAreaKey: textArea.value,
+    };
+    localStorage.setItem('formValues', JSON.stringify(formDataObject));
+  });
+});
+
+window.addEventListener('load', () => {
+  const formInfo = JSON.parse(localStorage.getItem('formValues'));
+  if (formInfo) {
+    fName.value = formInfo.nameKey;
+    email.value = formInfo.emailKey;
+    textArea.value = formInfo.textAreaKey;
+  } else {
+    fName.value = '';
+    email.value = '';
+    textArea.value = '';
+  }
+});
+
+const validate = () => {
+  const regx = /^([a-z\d-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
+  form.addEventListener('submit', (e) => {
+    const formInfo = JSON.parse(localStorage.getItem('formValues'));
+    const emailAddress = formInfo.emailKey;
+    if (!(regx.test(emailAddress))) {
+      message.innerHTML = 'Enter valid email address! in lowercase; abc@gmail.com';
+      e.preventDefault();
+    }
+  });
+};
+
+button.addEventListener('click', validate());
 let displayCard = '';
 for (let i = 0; i < cardsData.length; i += 1) {
   displayCard += `
   
   <article class="works_article bg_color7">
+
             <figure class="figure1">
+
                 <a href="#"><img src="${cardsData[i].image[0]}" alt="Tonic_img"></a>
+
             </figure>
+
             <figure class="figure2">
+
                 <a href="#"><img src="${cardsData[i].image[1]}" alt="Nature_img"></a>
+
             </figure>
+
             <div class="works_articlediv show1">
+
                 <h3><a class="textdecoration font1" href="#">${cardsData[i].name[0]}</a></h3> 
+
                 <ul>
+
                     <li><a class="textdecoration font2" href="#">CANOPY</a></li>
                     <li><a class="textdecoration" href="#"><img src="media/mid_dot.jpg" alt="mit_dot"></a></li>
                     <li><a class="textdecoration font3" href="#">Back End Dev</a></li>
                     <li class="textdecoration"><a href="#"><img src="media/mid_dot.jpg" alt="mit_dot"></a></li>
                     <li><a class="textdecoration font3" href="#">2015</a></li>
+
+
                 </ul> 
-                <p>${cardsData[i].description[0]}</p>
+
+
+                <p>${cardsData[i].description[0].substring(0, 85)}</p>
+
                 <ul class="gridbox1">
+
                     <li class="background1"><a class="textdecoration font4" href="#">${cardsData[i].technologies[0]}</a></li>
                     <li class="background1"><a class="textdecoration font4" href="#">${cardsData[i].technologies[1]}</a></li>
                     <li class="background1"><a class="textdecoration font4" href="#">${cardsData[i].technologies[3]}</a></li>
+
                 </ul>
+
                 <button class="font5 textdecoration seepopup" type="button" index=${cardsData[i].id}>See Project</button>
+
             </div>
+
             <div class="works_articlediv show2">
+
                 <h3><a class="textdecoration font1" href="#">${cardsData[i].name[0]}</a></h3>
+
                 <ul class="ul1">
+
                     <li><a class="textdecoration font2" href="#">CANOPY</a></li>
                     <li><a class="textdecoration" href="#"><img src="media/mid_dot.jpg" alt=""></a></li>
                     <li><a class="textdecoration font3" href="#">Back End Dev</a></li>
                     <li><a class="textdecoration" href="#"><img src="media/mid_dot.jpg" alt=""></a></li>
                     <li><a class="textdecoration font3" href="#">2015</a></li>
+
+
                 </ul>
+
                 <p>${cardsData[i].description[1]}</p>
+
                 <ul class="gridbox1">
+
                     <li class="background1"><a class="textdecoration font4" href="#">${cardsData[i].technologiesDesk[0]}</a></li>
                     <li class="background1"><a class="textdecoration font4" href="#">${cardsData[i].technologiesDesk[1]}</a></li>
                     <li class="background1"><a class="textdecoration font4" href="#">${cardsData[i].technologiesDesk[2]}</a></li>
                     <li class="background1"><a class="textdecoration font4" href="#">${cardsData[i].technologiesDesk[3]}</a></li>
+
                 </ul>
+
                 <button class="font5 textdecoration seepopup" type="button" index=${cardsData[i].id}>See Project</button>
+
             </div>
+
         </article>
+
   `;
 }
 
@@ -135,85 +213,121 @@ showPopupBtn.forEach((e) => {
 
     const popupEle = `
   <article class="popup_article bg_color7">
+
 <div class="popup_articlediv show1">
+
     <header class="popup_flex1">
+
         <h3><a class="textdecoration font1" href="#">${cardsData[btnIndex].name[0]}</a></h3>
+
         <button type="button" class="close_popup "><img src="media/closepopup.png" alt=""></button>
+
     </header>
+
     <ul class="list1">
+
         <li><a class="textdecoration font2" href="#">CANOPY</a></li>
         <li><a class="textdecoration" href="#"><img src="media/mid_dot.jpg" alt="mit_dot"></a></li>
         <li><a class="textdecoration font3" href="#">Back End Dev</a></li>
         <li class="textdecoration"><a href="#"><img src="media/mid_dot.jpg" alt="mit_dot"></a></li>
         <li><a class="textdecoration font3" href="#">2015</a></li>
+
+
     </ul>
+
     <figure class="figure1">
+
         <a href="#"><img src="${cardsData[btnIndex].image[0]}" alt="Tonic_img"></a>
+
     </figure>
+
     <article class="desc1">
         <p>${cardsData[btnIndex].description[0]}</p>
+
         <ul class="gridbox1 gridbox1b">
+
             <li class="background1"><a class="textdecoration font4" href="#">${cardsData[btnIndex].technologies[0]}</a></li>
             <li class="background1"><a class="textdecoration font4" href="#">${cardsData[btnIndex].technologies[1]}</a></li>
             <li class="background1"><a class="textdecoration font4" href="#">${cardsData[btnIndex].technologies[2]}</a></li>
+
         </ul>
     </article>
+
     <footer>
         <button class="footer_btn textdecoration" id="see-projects-btn" type="submit">
             <span>See Source</span>
             <img src="media/live_icon.png" alt="">
         </button>
+
         <button class="footer_btn textdecoration" id="see-projects-btn" type="submit">
             <span>See Live</span>
             <img src="media/github_source.jpg" alt="">
         </button>
         
     </footer>
+
 </div>
+
 <div class="popup_articlediv show2">
+
     <header class="popup_flex1">
+
         <h3><a class="textdecoration font1" href="#">${cardsData[btnIndex].name[0]}</a></h3>
+
         <button type="button" class="close_popup"><img src="media/closepopup.png" alt=""></button>
+
     </header> 
+
     <ul class="list1 ul1">
+
         <li><a class="textdecoration font2" href="#">CANOPY</a></li>
         <li><a class="textdecoration" href="#"><img src="media/mid_dot.jpg" alt=""></a></li>
         <li><a class="textdecoration font3" href="#">Back End Dev</a></li>
         <li><a class="textdecoration" href="#"><img src="media/mid_dot.jpg" alt=""></a></li>
         <li><a class="textdecoration font3" href="#">2015</a></li>
+
     </ul>
+
     <figure class="figure2 ">
+
         <a href="#"><img src="${cardsData[btnIndex].image[1]}" alt="Nature_img"></a>
+
     </figure>
+
     <article class="popup_flex2">
         <p class="width80">${cardsData[btnIndex].description[1]}</p>
+
         <div>
+
             <ul class="gridbox2">
+
                 <li class="background1"><a class="textdecoration font4" href="#">${cardsData[btnIndex].technologiesPop[0]}</a></li>
                 <li class="background1"><a class="textdecoration font4" href="#">${cardsData[btnIndex].technologiesPop[1]}</a></li>
                 <li class="background1"><a class="textdecoration font4" href="#">${cardsData[btnIndex].technologiesPop[2]}</a></li>
                 <li class="background1"><a class="textdecoration font4" href="#">${cardsData[btnIndex].technologiesPop[3]}</a></li>
                 <li class="background1"><a class="textdecoration font4" href="#">${cardsData[btnIndex].technologiesPop[4]}</a></li>
                 <li class="background1"><a class="textdecoration font4" href="#">${cardsData[btnIndex].technologiesPop[5]}</a></li>
+
             </ul>
+
             <footer>
-                 <button class="align_3 footer_btn textdecoration" id="see-projects-btn" type="submit">
-                    <span>See Live</span>
-                    <img src="media/live_icon.png" alt="">
-                </button>
                 <button class="align_3 footer_btn textdecoration" id="see-projects-btn" type="submit">
                     <span>See Source</span>
+                    <img src="media/live_icon.png" alt="">
+                </button>
+
+                <button class="align_3 footer_btn textdecoration" id="see-projects-btn" type="submit">
+                    <span>See Live</span>
                     <img src="media/github_source.jpg" alt="">
                 </button>
-                
             </footer>
             
         </div>
        
     </article>
- 
-    
+
 </div>
+
 </article>
 `;
 
